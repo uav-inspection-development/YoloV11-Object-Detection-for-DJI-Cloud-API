9273cf9ab71171e940414e2ff381300a522c255e
Phillweston <2436559745@qq.com>
2025-07-04 23:52:30 +0800

    Change the option to Chinese

--- src/web.py
+++ src/web.py
@@ -1553,7 +1553,7 @@ class Detection_UI:
                             corrected_image = enhance_texture(
                                 corrected_image, method="clahe"
                             )
-                        elif self.image_enhancement_method == "Histogram Equalization":
+                        elif self.image_enhancement_method == "直方图均衡化":
                             corrected_image = enhance_texture(
                                 corrected_image, method="histogram_equalization"
                             )
@@ -1636,7 +1636,7 @@ class Detection_UI:
 
                 if self.image_enhancement_method == "CLAHE":
                     corrected_image = enhance_texture(corrected_image, method="clahe")
-                elif self.image_enhancement_method == "Histogram Equalization":
+                elif self.image_enhancement_method == "直方图均衡化":
                     corrected_image = enhance_texture(
                         corrected_image, method="histogram_equalization"
                     )
@@ -1959,7 +1959,7 @@ class Detection_UI:
         if hasattr(self, "image_enhancement_method"):
             if self.image_enhancement_method == "CLAHE":
                 processed = enhance_texture(processed, method="clahe")
-            elif self.image_enhancement_method == "Histogram Equalization":
+            elif self.image_enhancement_method == "直方图均衡化":
                 processed = enhance_texture(processed, method="histogram_equalization")
 
         # 伪彩色处理
